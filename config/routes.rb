0e6f278d445e509198c1085b3655bf5b223d223f
vagrant <vagrant@localhost.localdomain>
2020-07-25 03:22:27 +0000

    [Finish] 5-6

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
     resource :favorites, only: [:create, :destroy]
     resources :post_comments, only: [:create, :destroy]
   end
-  resources :users, only: [:show, :edit]
+  resources :users, only: [:show, :edit, :update]
   devise_for :users
 end
